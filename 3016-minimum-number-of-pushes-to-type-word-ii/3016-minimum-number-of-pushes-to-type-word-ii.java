a7a7550a8b59a763c23c5b37d3b8ee0d9caed342
AlokYadavIIIT <yalok2372@gmail.com>
2026-07-31 19:36:34 +0530

    Time: 65 ms (10.69%), Space: 48 MB (52.6%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -17,17 +17,17 @@ class Solution {
         }
         Arrays.sort(arr);
 
-        for (int i = diff - 1; i >= 0; i--) {
-            if (i > diff - 9) {
-                res += arr[i];
-            } else if (i > diff - 17) {
-                res += arr[i] * 2;
-            } else if (i > diff - 25) {
-                res += arr[i] * 3;
-            } else {
-                res += arr[i] * 4;
-            }
-        }
+        // for (int i = diff - 1; i >= 0; i--) {
+        //     if (i > diff - 9) {
+        //         res += arr[i];
+        //     } else if (i > diff - 17) {
+        //         res += arr[i] * 2;
+        //     } else if (i > diff - 25) {
+        //         res += arr[i] * 3;
+        //     } else {
+        //         res += arr[i] * 4;
+        //     }
+        // }
         // for(int i=diff-1;i>=0;i--){
         //     int rank=diff-1-i;//0,1,2,..
         //     if(rank<8){
@@ -44,50 +44,50 @@ class Solution {
         //     }
         // }
 
-        // if(diff<=8){
-        //     for(int i=diff-1;i>-1;i--){
-        //         res+=arr[i]*1;
-        //     }
-        // }
-        // else if(diff>8 && diff<=16){
-        //     for(int i=diff-1;i>-1;i--){
-        //         if(i>diff-9){
-        //             res+=arr[i]*1;
-        //         }
-        //         else{
-        //             res+=arr[i]*2;
-        //         }
-        //     }
-        // }
-        // else if(diff>16 && diff<=24){
-        //     for(int i=diff-1;i>-1;i--){
-        //         if(i>diff-9){
-        //             res+=arr[i]*1;
-        //         }
-        //         else if(i>diff-17){
-        //             res+=arr[i]*2;
-        //         }
-        //         else{
-        //             res+=arr[i]*3;
-        //         }
-        //     }
-        // }
-        // else{
-        //     for(int i=diff-1;i>-1;i--){
-        //         if(i>diff-9){
-        //             res+=arr[i]*1;
-        //         }
-        //         else if(i>diff-17){
-        //             res+=arr[i]*2;
-        //         }
-        //         else if(i>diff-25){
-        //             res+=arr[i]*3;
-        //         }
-        //         else{
-        //             res+=arr[i]*4;
-        //         }
-        //     }
-        // } 
+        if(diff<=8){
+            for(int i=diff-1;i>-1;i--){
+                res+=arr[i]*1;
+            }
+        }
+        else if(diff>8 && diff<=16){
+            for(int i=diff-1;i>-1;i--){
+                if(i>diff-9){
+                    res+=arr[i]*1;
+                }
+                else{
+                    res+=arr[i]*2;
+                }
+            }
+        }
+        else if(diff>16 && diff<=24){
+            for(int i=diff-1;i>-1;i--){
+                if(i>diff-9){
+                    res+=arr[i]*1;
+                }
+                else if(i>diff-17){
+                    res+=arr[i]*2;
+                }
+                else{
+                    res+=arr[i]*3;
+                }
+            }
+        }
+        else{
+            for(int i=diff-1;i>-1;i--){
+                if(i>diff-9){
+                    res+=arr[i]*1;
+                }
+                else if(i>diff-17){
+                    res+=arr[i]*2;
+                }
+                else if(i>diff-25){
+                    res+=arr[i]*3;
+                }
+                else{
+                    res+=arr[i]*4;
+                }
+            }
+        } 
         return res;  
     }
 }
