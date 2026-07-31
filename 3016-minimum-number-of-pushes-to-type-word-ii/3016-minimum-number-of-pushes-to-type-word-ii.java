69ae8da263e8fbcbe7a5d58d4715b267a8ac5dba
AlokYadavIIIT <yalok2372@gmail.com>
2026-07-31 19:35:56 +0530

    Time: 70 ms (5.49%), Space: 48.3 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -16,21 +16,34 @@ class Solution {
             arr[idx++]=count;
         }
         Arrays.sort(arr);
-        for(int i=diff-1;i>=0;i--){
-            int rank=diff-1-i;//0,1,2,..
-            if(rank<8){
-                res+=arr[i];
-            }
-            else if(rank<16){
-                res+=arr[i]*2;
-            }
-            else if(rank<24){
-                res+=arr[i]*3;
-            }
-            else{
-                res+=arr[i]*4;
+
+        for (int i = diff - 1; i >= 0; i--) {
+            if (i > diff - 9) {
+                res += arr[i];
+            } else if (i > diff - 17) {
+                res += arr[i] * 2;
+            } else if (i > diff - 25) {
+                res += arr[i] * 3;
+            } else {
+                res += arr[i] * 4;
             }
         }
+        // for(int i=diff-1;i>=0;i--){
+        //     int rank=diff-1-i;//0,1,2,..
+        //     if(rank<8){
+        //         res+=arr[i];
+        //     }
+        //     else if(rank<16){
+        //         res+=arr[i]*2;
+        //     }
+        //     else if(rank<24){
+        //         res+=arr[i]*3;
+        //     }
+        //     else{
+        //         res+=arr[i]*4;
+        //     }
+        // }
+
         // if(diff<=8){
         //     for(int i=diff-1;i>-1;i--){
         //         res+=arr[i]*1;
