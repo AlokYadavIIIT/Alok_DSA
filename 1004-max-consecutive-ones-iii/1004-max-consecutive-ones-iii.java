1666fbd7098ec083d747ce2900219911e7d7e985
AlokYadavIIIT <yalok2372@gmail.com>
2026-07-31 20:03:46 +0530

    Time: 3 ms (95.95%), Space: 52.4 MB (18.16%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,28 +1,48 @@
 class Solution {
-    public int find(int[] nums,int low,int high){
-        int res =0;
-        for(int i=low;i<=high;i++){
-            if(nums[i]==1){
-                res++;
-            }
-        }
-        return res;
-    }
+    // public int find(int[] nums,int low,int high){
+    //     int res =0;
+    //     for(int i=low;i<=high;i++){
+    //         if(nums[i]==1){
+    //             res++;
+    //         }
+    //     }
+    //     return res;
+    // }
     public int longestOnes(int[] nums, int k) {
-        int n=nums.length;
-        int low=0,res=0;
-        for(int high=0;high<n;high++){
-            int maxone=find(nums,low,high);
-            int window= high-low+1;
-            int noOfZero=window-maxone;
-            while(noOfZero>k){
-                low++;
-                maxone=find(nums,low,high);
-                window=high-low+1;
-                noOfZero=window-maxone;
+        // int n=nums.length;
+        // int low=0,res=0;
+        // for(int high=0;high<n;high++){
+        //     int maxone=find(nums,low,high);
+        //     int window= high-low+1;
+        //     int noOfZero=window-maxone;
+        //     while(noOfZero>k){
+        //         low++;
+        //         maxone=find(nums,low,high);
+        //         window=high-low+1;
+        //         noOfZero=window-maxone;
+        //     }
+        //     res=Math.max(res,high-low+1);
+        // }
+
+
+        int low=0;
+        int zeros=0;
+        int res=0;
+        for(int high=0;high<nums.length;high++){
+            if(nums[high]==0){
+                zeros++;
             }
+
+            while(zeros>k){
+                if(nums[low]==0){
+                    zeros--;
+                }
+                low++;
+            }
+
             res=Math.max(res,high-low+1);
         }
+        
         return res;
     }
 }
