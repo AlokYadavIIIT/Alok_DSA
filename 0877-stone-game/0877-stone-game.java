3e57554c80c3879c438b741bdbf0a5d6f53af946
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-02 18:04:33 +0530

    Time: 0 ms (100%), Space: 43 MB (62.03%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+
+
+
+// class Solution {
+//     Integer[][] memo;
+
+//     public boolean predictTheWinner(int[] piles) {
+//         int n=piles.length;
+//         memo = new Integer[n][n];
+
+//         return solve(piles,0,n-1)>0;
+//     }
+
+//     private int solve(int[] piles,int i,int j){
+//         if(i==j){
+//             return piles[i];
+//         }
+
+//         if(memo[i][j]!=null){
+//             return memo[i][j];
+//         }
+
+//         int takeleft = piles[i]-solve(piles,i+1,j);
+//         int takeRight = piles[j]-solve(piles,i,j-1);
+
+//         return memo[i][j] = Math.max(takeleft,takeRight);
+//     }
+// }
