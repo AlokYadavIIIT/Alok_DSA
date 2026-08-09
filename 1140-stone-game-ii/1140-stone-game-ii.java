3fdc0d6db5ea8dcc9bb9b7c4985e357c4749325a
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-09 22:35:56 +0530

    Time: 4 ms (73.16%), Space: 44.1 MB (88.69%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -15,19 +15,15 @@ class Solution {
         return solve(0, 1);
     }
     public int solve(int i, int M) {
-
         // All piles are taken
         if (i >= suffix.length - 1) {
             return 0;
         }
-
         // Already calculated
         if (dp[i][M] != 0) {
             return dp[i][M];
         }
-
         int ans = 0;
-
         // Try taking X piles
         for (int X = 1; X <= 2 * M && i + X <= suffix.length - 1; X++) {
 
@@ -39,7 +35,6 @@ class Solution {
 
             ans = Math.max(ans, currentPlayer);
         }
-
         dp[i][M] = ans;
         return ans;
     }
