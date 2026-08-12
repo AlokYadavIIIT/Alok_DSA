866b62a9bd265030d6bcca53a9e2a32ce7fba717
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-12 19:30:26 +0530

    Time: 23 ms (85.16%), Space: 65.5 MB (60.09%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int n=nums.length;
+        int z=0;
+        int o=0;
+        int res=0;
+        HashMap<Integer,Integer> f = new HashMap<>();
+        f.put(0,-1);//when z=o;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                z++;
+            }else{
+                o++;
+            }
+            int diff = z-o;
+            // int ques = f.getOrDefault(diff,0);
+            if(f.containsKey(diff)){
+                int firstIndex = f.get(diff);
+                res=Math.max(res,i-firstIndex);
+            }else{
+                f.put(diff,i);
+            }
+            
+        }
+        return res;
+    }
+}
