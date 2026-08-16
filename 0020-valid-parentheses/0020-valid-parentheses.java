2229a9edbee832d32784362ba0203a40371c5e01
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-16 18:21:54 +0530

    Time: 4 ms (36.7%), Space: 43.3 MB (41.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char c = s.charAt(i);
+            //opening bracket
+            if(c=='('|| c=='{' || c=='['){
+                st.push(c);
+            }//closing brackets.
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(c==')' && st.peek()=='('){
+                    st.pop();
+                }
+                else if(c=='}' && st.peek()=='{'){
+                    st.pop();
+                }
+                else if(c==']' && st.peek()=='['){
+                    st.pop();
+                }else if(c!=st.peek()){
+                    return false;
+                }
+            }
+        }
+        if(!st.isEmpty()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
