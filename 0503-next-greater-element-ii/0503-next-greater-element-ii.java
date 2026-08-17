15714941c896238a743853b12b707da96d2b9084
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-17 19:22:45 +0530

    Time: 13 ms (73.74%), Space: 48.7 MB (23.15%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,27 +1,58 @@
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int n = nums.length;
+//         int[] res = new int[n];
+
+//         Arrays.fill(res, -1);
+
+//         Stack<Integer> st = new Stack<>();
+// //Here for the circular we assume it like traversing 2 times the same array from back;
+//         for (int i = 2 * n - 1; i >= 0; i--) {
+
+//             int index = i % n;
+
+//             while (!st.isEmpty() && st.peek() <= nums[index]) {
+//                 st.pop();
+//             }
+// //Here we removed the isEmpty() condition bcz we have already make -1 to all the res value .
+//             if (!st.isEmpty()) {
+//                 res[index] = st.peek();
+//             }
+
+//             st.push(nums[index]);
+//         }
+
+//         return res;
+//     }
+// }
+
+
+//METHOD-2 : FOR CIRCULAR ARRAY
+
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
+        int n=nums.length;
         int[] res = new int[n];
 
-        Arrays.fill(res, -1);
+        Stack<Integer> st =new Stack<>();
+        for(int i=n-2;i>=0;i--){
+            st.push(nums[i]);
+        }
 
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = 2 * n - 1; i >= 0; i--) {
-
-            int index = i % n;
-
-            while (!st.isEmpty() && st.peek() <= nums[index]) {
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=nums[i]){
                 st.pop();
             }
 
-            if (!st.isEmpty()) {
-                res[index] = st.peek();
+            if(st.isEmpty()){
+                res[i]=-1;
             }
-
-            st.push(nums[index]);
+            else{
+                res[i]=st.peek();
+            }
+            st.push(nums[i]);
         }
-
         return res;
     }
 }
