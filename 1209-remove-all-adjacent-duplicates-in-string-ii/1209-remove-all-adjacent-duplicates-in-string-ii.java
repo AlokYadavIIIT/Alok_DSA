5e287eab46fc87f1f01ed616b7ea7254cc5c15c2
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-18 18:32:20 +0530

    Time: 32 ms (36.71%), Space: 47 MB (80.33%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,47 +1,95 @@
-class Solution {
-    class Pair{
-        char first;
-        int second;
+//METHOD-1:
+// class Solution {
+//     class Pair{
+//         char first;
+//         int second;
 
-        Pair(char first,int second){
-            this.first=first;
-            this.second=second;
+//         Pair(char first,int second){
+//             this.first=first;
+//             this.second=second;
+//         }
+//     }
+//     public String removeDuplicates(String s, int k) {
+//         int n=s.length();
+//         Stack<Pair> st = new Stack<>();
+        
+//         for(int i=0;i<n;i++){
+//             char c = s.charAt(i);
+//             if(st.isEmpty()){
+//                 st.push(new Pair(c,1));
+//                 continue;
+//             }
+//             else{
+//                 if(st.peek().first!=c){
+//                 st.push(new Pair(c,1));
+//                 continue;
+//                 }
+//                 else if(st.peek().first==c && st.peek().second<(k-1)){
+//                     Pair p = st.peek();
+//                     st.pop();
+//                     st.push(new Pair(p.first,p.second+1));
+//                     continue;
+//                 }
+//                 else{
+//                     st.pop();
+//                 }
+//             }    
+//         }
+//         StringBuilder sb = new StringBuilder();
+//         while(!st.isEmpty()){
+//             Pair p = st.peek();
+//             st.pop();
+//             while(p.second-->0){
+//                 sb.append(p.first);
+//             }
+//         }
+//         return sb.reverse().toString();
+//     }
+// }
+
+
+//METHOD-2:
+class Solution {
+
+    class Pair {
+        char ch;
+        int count;
+
+        Pair(char ch, int count) {
+            this.ch = ch;
+            this.count = count;
         }
     }
+
     public String removeDuplicates(String s, int k) {
-        int n=s.length();
+
         Stack<Pair> st = new Stack<>();
-        
-        for(int i=0;i<n;i++){
-            char c = s.charAt(i);
-            if(st.isEmpty()){
-                st.push(new Pair(c,1));
-                continue;
-            }
-            else{
-                if(st.peek().first!=c){
-                st.push(new Pair(c,1));
-                continue;
-                }
-                else if(st.peek().first==c && st.peek().second<(k-1)){
-                    Pair p = st.peek();
-                    st.pop();
-                    st.push(new Pair(p.first,p.second+1));
-                    continue;
-                }
-                else{
+
+        for (char c : s.toCharArray()) {
+
+            if (!st.isEmpty() && st.peek().ch == c) {
+
+                st.peek().count++;
+
+                if (st.peek().count == k) {
                     st.pop();
                 }
-            }    
-        }
-        StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty()){
-            Pair p = st.peek();
-            st.pop();
-            while(p.second-->0){
-                sb.append(p.first);
+
+            } else {
+                st.push(new Pair(c, 1));
             }
         }
-        return sb.reverse().toString();
+
+        StringBuilder ans = new StringBuilder();
+
+        while (!st.isEmpty()) {
+            Pair p = st.pop();
+
+            for (int i = 0; i < p.count; i++) {
+                ans.append(p.ch);
+            }
+        }
+
+        return ans.reverse().toString();
     }
 }
