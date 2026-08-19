4e434a50d176ac5430e3afb266fde49e238ef5cf
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-19 19:37:07 +0530

    Time: 173 ms (5.15%), Space: 154 MB (5.3%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+    Map<Integer,Integer> dp;
+    int sol(int[] a,int i){
+        if(i>=a.length)return 0;
+
+        if(dp.containsKey(i)){
+            return dp.get(i);
+        }
+        int diff=Integer.MIN_VALUE;
+        int sum=0;
+        int prev=0;
+        for(int j=i;j<i+3 && j<a.length;j++){
+            prev+=a[j];
+            sum=prev-sol(a,j+1);
+            diff=Math.max(diff,sum);
+        }
+        dp.put(i,diff);
+        return diff;
+    }
+    public String stoneGameIII(int[] stoneValue) {
+        dp=new HashMap<>();
+        int score=sol(stoneValue,0);
+        if(score>0){
+            return "Alice";
+        }
+        else if(score<0){
+            return "Bob";
+        }
+        else{
+            return "Tie";
+        }
+    }
+}
