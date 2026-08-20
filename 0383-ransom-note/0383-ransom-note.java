a2b1cdbd593a06c630b8cf83654d0913ee3e7862
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-20 19:14:19 +0530

    Time: 16 ms (19.36%), Space: 47.3 MB (7.72%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,24 +1,57 @@
+// //METHOD-1:
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         int n = magazine.length();
+
+//         HashMap<Character,Integer> freq = new HashMap<>();
+
+//         for(int i=0;i<n;i++){
+//             char c = magazine.charAt(i);
+//             freq.put(c,freq.getOrDefault(c,0)+1);
+//         }
+
+//         for(int i=0;i<ransomNote.length();i++){
+//             char c = ransomNote.charAt(i);
+            
+//             if(freq.getOrDefault(c,0)==0){
+//                 return false;
+//             }
+
+//             freq.put(c,freq.get(c)-1);
+//         }
+    
+//         return true;
+//     }
+// }
+
+
+//METHOD-2:
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int n = magazine.length();
 
-        HashMap<Character,Integer> freq = new HashMap<>();
+        HashMap<Character,Integer> freq1 = new HashMap<>();
 
         for(int i=0;i<n;i++){
             char c = magazine.charAt(i);
-            freq.put(c,freq.getOrDefault(c,0)+1);
+            freq1.put(c,freq1.getOrDefault(c,0)+1);
         }
 
-        for(int i=0;i<ransomNote.length();i++){
-            char c = ransomNote.charAt(i);
-            
-            if(freq.getOrDefault(c,0)==0){
-                return false;
-            }
+        int m = ransomNote.length();
+        HashMap<Character,Integer> freq2 = new HashMap<>();
 
-            freq.put(c,freq.get(c)-1);
+        for(int i=0;i<m;i++){
+            char c = ransomNote.charAt(i);
+            freq2.put(c,freq2.getOrDefault(c,0)+1);
         }
     
+        for(int i=0;i<m;i++){
+            char c = ransomNote.charAt(i);
+
+            if(freq1.getOrDefault(c,0)<freq2.get(c)){
+                return false;
+            }
+        }
         return true;
     }
 }
