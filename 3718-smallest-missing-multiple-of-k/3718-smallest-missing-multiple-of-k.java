43446ec4b9c4e49010aab19f2876fd1c30e7986d
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-25 10:10:52 +0530

    Time: 2 ms (68.2%), Space: 45.3 MB (52.43%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,35 +1,57 @@
+//METHOD-1:
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        
         int n = nums.length;
-        ArrayList<Integer> quotient = new ArrayList<>();
 
-        int max = nums.length+1;
-
-        for(int i=0;i<n;i++){
-            if(max<nums[i] && nums[i]%k==0)
-                max=nums[i];
-        }
-//here we use i<=max/k bcz it starts from 0 and 0 never be any quotient 
-// +1 because quotient itself is used as an index
-        for(int i=0;i<=max/k;i++){
-            quotient.add(0);
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
         }
 
-        for(int i=0;i<n;i++){
-            int quot = nums[i]/k;
-
-            if(quot !=0 && nums[i]%k ==0 ){
-                quotient.set(quot,1);
-            }
+        int multiple=k;
+        while(set.contains(multiple)){
+            multiple+=k;
         }
 
-        for(int i=1;i<quotient.size();i++){
-            if(quotient.get(i)==0){
-                return k*(i);
-            }
-        }
-
-        return k*(quotient.size());
+        return multiple;
     }
 }
+
+
+
+//METHOD -2 
+// class Solution {
+//     public int missingMultiple(int[] nums, int k) {
+        
+//         int n = nums.length;
+//         ArrayList<Integer> quotient = new ArrayList<>();
+
+//         int max = nums.length+1;
+
+//         for(int i=0;i<n;i++){
+//             if(max<nums[i] && nums[i]%k==0)
+//                 max=nums[i];
+//         }
+// //here we use i<=max/k bcz it starts from 0 and 0 never be any quotient 
+// // +1 because quotient itself is used as an index
+//         for(int i=0;i<=max/k;i++){
+//             quotient.add(0);
+//         }
+
+//         for(int i=0;i<n;i++){
+//             int quot = nums[i]/k;
+
+//             if(quot !=0 && nums[i]%k ==0 ){
+//                 quotient.set(quot,1);
+//             }
+//         }
+
+//         for(int i=1;i<quotient.size();i++){
+//             if(quotient.get(i)==0){
+//                 return k*(i);
+//             }
+//         }
+
+//         return k*(quotient.size());
+//     }
+// }
