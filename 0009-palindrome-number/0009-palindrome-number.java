65217f685bfd9f5019eccd36ba94874df745e327
AlokYadavIIIT <yalok2372@gmail.com>
2026-08-29 09:55:09 +0530

    Time: 5 ms (82.1%), Space: 45.3 MB (99.47%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,41 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+        
+//         String s = String.valueOf(x);
+//         int n = s.length();
+//         int left=0,right=n-1;
+//         while(left<=right){
+//             char l = s.charAt(left);
+//             char r = s.charAt(right);
+//             if(l==r){
+//                 left++;
+//                 right--;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
 class Solution {
     public boolean isPalindrome(int x) {
         
-        String s = String.valueOf(x);
-        int n = s.length();
-        int left=0,right=n-1;
-        while(left<=right){
-            char l = s.charAt(left);
-            char r = s.charAt(right);
-            if(l==r){
-                left++;
-                right--;
-            }
-            else{
-                return false;
-            }
+          if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return true;
+
+        int reverse = 0;
+
+        while (x > reverse) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reverse || x == reverse / 10;
     }
 }
