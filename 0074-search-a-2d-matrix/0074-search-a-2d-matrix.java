d52a82bff861c28c0af07fcf7c3e520882819b7c
AlokYadavIIIT <yalok2372@gmail.com>
2026-09-16 18:24:23 +0530

    Time: 0 ms (100%), Space: 43.7 MB (90.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,40 +1,74 @@
+
+//METHOD -1 : TWO BINARY SEARCH USES
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         //compare with first element of each row(a[i][0]<=target) like search for 16 then after comparison shows {yes,yes , no ...}whenever first no comes then it say may be target exist in just prev of no. giving row.
+
+//         int m=matrix.length;
+//         int n=matrix[0].length;
+
+//         if(target<matrix[0][0] || target>matrix[m-1][n-1]){
+//             return false;
+//         }
+
+//         int low=0;
+//         int high=m-1;
+//         int row=-1;
+//         while(low<=high){
+//             int guess=low+(high-low)/2;
+
+//             if(matrix[guess][0]<=target){
+//                 row=guess;
+//                 low=guess+1;
+//             }else{
+//                 high=guess-1;
+//             }
+//         }
+
+//         int rowlow=0,rowhigh=n-1;
+//         while(rowlow<=rowhigh){
+//             int guess=rowlow+(rowhigh-rowlow)/2;
+
+//             if(matrix[row][guess]==target){
+//                 return true;
+//             }
+//             else if(matrix[row][guess]<target){
+//                 rowlow=guess+1;
+//             }
+//             else{
+//                 rowhigh=guess-1;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
+
+//METHOD-2:ONE BINARY SEARCH USES
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //compare with first element of each row(a[i][0]<=target) like search for 16 then after comparison shows {yes,yes , no ...}whenever first no comes then it say may be target exist in just prev of no. giving row.
-
+        
         int m=matrix.length;
         int n=matrix[0].length;
 
-        if(target<matrix[0][0] || target>matrix[m-1][n-1]){
-            return false;
-        }
+        int low=0,high=m*n-1;
 
-        int low=0;
-        int high=m-1;
-        int row=-1;
         while(low<=high){
             int guess=low+(high-low)/2;
 
-            if(matrix[guess][0]<=target){
-                row=guess;
-                low=guess+1;
-            }else{
-                high=guess-1;
-            }
-        }
+            int row=guess/n;
+            int col=guess%n;
 
-        int rowlow=0,rowhigh=n-1;
-        while(rowlow<=rowhigh){
-            int guess=rowlow+(rowhigh-rowlow)/2;
-
-            if(matrix[row][guess]==target){
+            if(matrix[row][col]==target){
                 return true;
             }
-            else if(matrix[row][guess]<target){
-                rowlow=guess+1;
+            else if(matrix[row][col]<target){
+                low=guess+1;
             }
             else{
-                rowhigh=guess-1;
+                high=guess-1;
             }
         }
 
