5cc15bc05385450eb6b56091a171d03f158bbcd9
AlokYadavIIIT <yalok2372@gmail.com>
2026-09-22 20:51:43 +0530

    Time: 33 ms (31.93%), Space: 53.7 MB (80.26%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,53 +1,87 @@
+// class Solution {
+
+//     public int[][] kClosest(int[][] points, int k) {
+
+//         HashMap<Integer, ArrayList<int[]>> map = new HashMap<>();
+
+//         PriorityQueue<Integer> maxheap =
+//             new PriorityQueue<>(Collections.reverseOrder());
+
+//         // Store points according to their distance
+//         for (int[] point : points) {
+
+//             int x = point[0];
+//             int y = point[1];
+
+//             int distance = x * x + y * y;
+
+//             // If distance is not present, create a new list
+//             map.putIfAbsent(distance, new ArrayList<>());
+
+//             // Add point to that distance's list
+//             map.get(distance).add(point);
+
+//             // Add distance to max heap
+//             maxheap.add(distance);
+
+//             // Keep only k distances
+//             if (maxheap.size() > k) {
+//                 maxheap.poll();
+//             }
+//         }
+
+//         int[][] ans = new int[k][2];
+
+//         // To keep track of which point of a particular distance
+//         // has already been taken
+
+//         HashMap<Integer, Integer> indexMap = new HashMap<>();
+
+//         for (int i = 0; i < k; i++) {
+
+//             int distance = maxheap.poll();
+
+//             ArrayList<int[]> list = map.get(distance);
+
+//             int index = indexMap.getOrDefault(distance, 0);
+
+//             ans[i] = list.get(index);
+
+//             indexMap.put(distance, index + 1);
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+//METHOD-2:
 class Solution {
 
     public int[][] kClosest(int[][] points, int k) {
 
-        HashMap<Integer, ArrayList<int[]>> map = new HashMap<>();
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a, b) -> {
+                int d1 = a[0] * a[0] + a[1] * a[1];
+                int d2 = b[0] * b[0] + b[1] * b[1];
 
-        PriorityQueue<Integer> maxheap =
-            new PriorityQueue<>(Collections.reverseOrder());
+                return d2 - d1;
+            }
+        );
 
-        // Store points according to their distance
         for (int[] point : points) {
 
-            int x = point[0];
-            int y = point[1];
+            maxHeap.add(point);
 
-            int distance = x * x + y * y;
-
-            // If distance is not present, create a new list
-            map.putIfAbsent(distance, new ArrayList<>());
-
-            // Add point to that distance's list
-            map.get(distance).add(point);
-
-            // Add distance to max heap
-            maxheap.add(distance);
-
-            // Keep only k distances
-            if (maxheap.size() > k) {
-                maxheap.poll();
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
             }
         }
 
         int[][] ans = new int[k][2];
 
-        // To keep track of which point of a particular distance
-        // has already been taken
-
-        HashMap<Integer, Integer> indexMap = new HashMap<>();
-
         for (int i = 0; i < k; i++) {
-
-            int distance = maxheap.poll();
-
-            ArrayList<int[]> list = map.get(distance);
-
-            int index = indexMap.getOrDefault(distance, 0);
-
-            ans[i] = list.get(index);
-
-            indexMap.put(distance, index + 1);
+            ans[i] = maxHeap.poll();
         }
 
         return ans;
