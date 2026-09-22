be92c7119bf332de81e7c88a5bd63631910c0129
AlokYadavIIIT <yalok2372@gmail.com>
2026-09-22 20:48:25 +0530

    Time: 79 ms (5.08%), Space: 61.9 MB (5.01%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public int[][] kClosest(int[][] points, int k) {
+
+        HashMap<Integer, ArrayList<int[]>> map = new HashMap<>();
+
+        PriorityQueue<Integer> maxheap =
+            new PriorityQueue<>(Collections.reverseOrder());
+
+        // Store points according to their distance
+        for (int[] point : points) {
+
+            int x = point[0];
+            int y = point[1];
+
+            int distance = x * x + y * y;
+
+            // If distance is not present, create a new list
+            map.putIfAbsent(distance, new ArrayList<>());
+
+            // Add point to that distance's list
+            map.get(distance).add(point);
+
+            // Add distance to max heap
+            maxheap.add(distance);
+
+            // Keep only k distances
+            if (maxheap.size() > k) {
+                maxheap.poll();
+            }
+        }
+
+        int[][] ans = new int[k][2];
+
+        // To keep track of which point of a particular distance
+        // has already been taken
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+
+        for (int i = 0; i < k; i++) {
+
+            int distance = maxheap.poll();
+
+            ArrayList<int[]> list = map.get(distance);
+
+            int index = indexMap.getOrDefault(distance, 0);
+
+            ans[i] = list.get(index);
+
+            indexMap.put(distance, index + 1);
+        }
+
+        return ans;
+    }
+}
