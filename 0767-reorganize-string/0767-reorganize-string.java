0b930baa5fda49c11b5312aced7472411095e078
AlokYadavIIIT <yalok2372@gmail.com>
2026-09-23 19:33:39 +0530

    Time: 9 ms (6.73%), Space: 44.8 MB (5.22%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,58 @@
+class Solution {
+    class pair{
+        int first;
+        char second;
+
+        pair(int f,char s){
+            first=f;
+            second=s;
+        }
+    }
+
+    public String reorganizeString(String s) {
+        
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        PriorityQueue<pair> pq=new PriorityQueue<>((a,b)->{
+            return b.first-a.first;
+        });
+
+        for(char key : map.keySet()){
+            pq.add(new pair(map.get(key),key));
+        }
+
+        String res = "";
+        int seat = 0;
+        while(!pq.isEmpty()){
+            pair p = pq.poll();
+            if(seat == 0 || res.charAt(seat-1)!=p.second){
+                res+=(p.second);
+                seat++;
+                p.first--;
+                if(p.first>0){
+                    pq.add(p);
+                }
+            }
+            else{
+                if(pq.isEmpty()){//it states like further elements as "..aaa"same;
+                    return "";
+                }
+                pair p2=pq.poll();
+                res+=(p2.second);
+                seat++;
+                p2.first--;
+                if(p2.first>0){
+                    pq.add(p2);
+                }
+            // Put p back because we didn't use it
+                pq.add(p);
+            }
+        }
+
+        return res;
+    }
+}
